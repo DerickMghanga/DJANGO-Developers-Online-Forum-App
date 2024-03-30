a06d56409444552cc8426e89890db8b2ca145614
DerickMghanga <derickmghanga3329@gmail.com>
2024-03-30 12:49:01 +0300

    update user details functionality and UI

--- studyMate/base/forms.py
+++ studyMate/base/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User #built-in db table Django Framework
 
 
 class RoomForm(ModelForm):
@@ -7,3 +8,9 @@ class RoomForm(ModelForm):
         model = Room
         fields = '__all__'  #creates form fields based on attributes in 'Room' model
         exclude = ['host', 'participants']  # exclude a field in the form
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
